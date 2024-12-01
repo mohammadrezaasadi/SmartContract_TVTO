4bad0d5097a200c9e170707c6232d21625de469c
mohammadrezaasadi <rsasadi@gmail.com>
2024-12-01 12:36:59 +0330

    Define constructor

--- Project01/Hello.sol
+++ Project01/Hello.sol
@@ -4,11 +4,15 @@ pragma solidity >=0.4.16 <0.9.0;
 contract SimpleStorage {
     //State Variable
     uint storedData;
+    
+    constructor(){
+        storedData=10;
+    }
 
     function set(uint x) public {
         storedData = x;
     }
-
+    
     function get() public view returns (uint) {
         return storedData;
     }
